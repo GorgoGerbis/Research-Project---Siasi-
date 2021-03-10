81020bd2e852ac45500439be682fb16fdb2f0e46
Jackson <jacksonwalker5121@gmail.com>
2021-03-10 00:49:28 -0500

    ALL NODES DISPLAYED NOW SHOW THE LINKS BETWEEN THEM

--- src/homeGUI.py
+++ src/homeGUI.py
@@ -50,15 +50,18 @@ def main():
     newNodeA = NodeObj(1, (80, 50), "A", (40, 40, 40), 1, 5)
     newNodeB = NodeObj(2, (140, 50), "A", (40, 40, 40), 1, 5)
     newNodeC = NodeObj(3, (240, 50), "A", (40, 40, 40), 1, 5)
-    newNodeD = NodeObj(5, (380, 50), "A", (40, 40, 40), 1, 5)
-    newNodeE = NodeObj(8, (550, 50), "A", (40, 40, 40), 1, 5)
+    newNodeD = NodeObj(4, (380, 50), "A", (40, 40, 40), 1, 5)
+    newNodeE = NodeObj(5, (550, 50), "A", (40, 40, 40), 1, 5)
+    newNodeF = NodeObj(6, (550, 50), "A", (40, 40, 40), 1, 5)
+    newNodeG = NodeObj(7, (550, 50), "A", (40, 40, 40), 1, 5)
+    newNodeH = NodeObj(8, (550, 50), "A", (40, 40, 40), 1, 5)
 
     newLinkA = LinkObj(1, 2, 100, 0.3, 5)
     newLinkB = LinkObj(1, 3, 100, 0.3, 5)
     newLinkC = LinkObj(1, 5, 100, 0.3, 5)
     newLinkD = LinkObj(1, 8, 100, 0.3, 5)
 
-    tempNodeList = [newNodeA, newNodeB, newNodeC, newNodeD, newNodeE]
+    tempNodeList = [newNodeA, newNodeB, newNodeC, newNodeD, newNodeE, newNodeF, newNodeG, newNodeH]
     tempLinkList = [newLinkA, newLinkB, newLinkC, newLinkD]
 
     for node in tempNodeList:
@@ -68,7 +71,8 @@ def main():
     # The loop proper, things inside this loop will
     # be called over and over until you exit the window
 
-    nodesDrawn = True
+    nodesDrawn = True # Flipped False when all nodes are drawn
+    linksDrawn = True
 
     while True:
         checkEvents()
@@ -76,30 +80,28 @@ def main():
         #drawLine()
         #drawRect()
         drawGrid(1)
+
         if nodesDrawn:
             for nodes in tempNodeList:
                 drawNode(nodes)
-#        drawLinks(tempLinkList)
+
+        if linksDrawn:
+            for link in tempLinkList:
+                drawLink(link, tempNodeList)
+
         pygame.display.update()
 
 
+def drawLink(link, tempNodeList):
+    for node in tempNodeList:
+        if node.nodeID == link.linkSrc:
+            startingNode = node
+        if node.nodeID == link.linkDest:
+            endingNode = node
 
-# def drawLinks(links):
-#     for link in links:
-#
-#         startingNode = link.linkSrc
-#         endingNode = link.linkDest
-#
-#         startingPos = 0
-#         endingPos = 0
-#
-#         for node in NodeObj.StaticNodeList:
-#             if node.nodeID == startingNode:
-#                 startingPos = node.nodePosition[0]
-#             if node.nodeID == endingNode:
-#                 endingPos = node.nodePosition[1]
-#
-#         pygame.draw.line(_VARS['surf'], BLACK, startingPos, endingPos)
+    pygame.draw.line(_VARS['surf'], BLACK, startingNode.nodePosition, endingNode.nodePosition)
+    # pygame.draw.line(screen, Color_line, (60, 80), (130, 100))
+    # pygame.display.flip()
 
 
 
@@ -117,10 +119,7 @@ def drawNode(node): # Parameter will be nodeObj
         rect = pygame.draw.rect(_VARS['surf'], WHITE, (node.nodePosition[0], node.nodePosition[1], 60, 20))
         _VARS['surf'].blit(txt_surface, (rect.x, rect.y))
 
-
         StaticCurrentDisplayedNodes.append(node)
-
-
 
 
 def drawGrid(divisions):
