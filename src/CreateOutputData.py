b4fb6ec79ce4d6e9dd52ad64561935b1f948a35c
jackson.walker.18 <jackson.walker.18@cnu.edu>
2022-04-25 09:29:05 -0400

    commit any changes I have made recently.

--- src/CreateOutputData.py
+++ src/CreateOutputData.py
@@ -15,7 +15,6 @@ REQUEST_APPROVED = 3
 
 AUTO_FAIL = []# [5, 6, 13, 19]
 
-
 #@Nvm it works as intended, Path one type paths will map on failed nodes and just be failed after while type two will avoid failed nodes completely.
 def fail_unavailable_paths():
     for req in Request.STATIC_TOTAL_REQUEST_LIST:
